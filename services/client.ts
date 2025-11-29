fa4594d3b78a8c423ac46e2b780e9c8e6737eb54
minhleeee123 <157082841+minhleeee123@users.noreply.github.com>
2025-11-30 00:21:41 +0700

    final commit

--- services/client.ts
+++
@@ -1,8 +0,0 @@
-
-import { GoogleGenAI } from "@google/genai";
-
-if (!process.env.API_KEY) {
-  console.error("API_KEY is missing from environment variables.");
-}
-
-export const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
